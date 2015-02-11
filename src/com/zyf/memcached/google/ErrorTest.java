7c4e80d326a9d70b806fd9a72a751b7d971fab07
yunfei.zang <yunfei.zang@jrj.com.cn>
2015-02-11 10:26:17 +0800

    调整测试程序

--- src/com/zyf/memcached/google/ErrorTest.java
+++ src/com/zyf/memcached/google/ErrorTest.java
@@ -3,18 +3,20 @@ package com.zyf.memcached.google;
 public class ErrorTest {
 	public static String value = "11111111111111111111111111111111111111111111111111111111";
 	private static SockIOPool pool;
+	static String key = "win_0E3D0B82339ECC23-992BA88F-855B-4CB8-AEE0-659116BD7DD6";
+	static MemCachedClient mc = new MemCachedClient("test");
 	static {
 		String[] serverlist = { "127.0.0.1:11211" };
 		pool = SockIOPool.getInstance("test");
 		pool.setServers(serverlist);
 		pool.initialize();
+
+		System.out.println("start set key:" + mc.set(key, value));
 	}
 
 	public static void main(String[] args) {
-		MemCachedClient mc = new MemCachedClient("test");
-		String key = "win_0E3D0B82339ECC23-992BA88F-855B-4CB8-AEE0-659116BD7DD6";
-		// System.out.println(mc.set(key, value));
-		System.out.println("=====================");
+		System.out
+				.println("====================get key========================");
 		System.out.println(mc.get(key));
 	}
 }
